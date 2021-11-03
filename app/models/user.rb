61b28231362cc32bf36e85b1dbbc1afc10f325b4
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-03 22:09:50 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,12 @@ class User < ApplicationRecord
 
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めてください'
   
-  with_option presence: true, format:{ with: ZENKAKU_REGEX, message: '全角文字を使用してください' } do
+  with_options presence: true, format:{ with: ZENKAKU_REGEX, message: '全角文字を使用してください' } do
     validates :family_name
     validates :first_name
   end
 
-  with_option presence: true, format:{ with: KANA_REGEX, message: '全角カナ文字を使用してください' } do
+  with_options presence: true, format:{ with: KANA_REGEX, message: '全角カナ文字を使用してください' } do
     validates :family_name_kana
     validates :first_name_kana
   end
