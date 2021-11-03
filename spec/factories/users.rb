feff1b4fdbfe1cff024d93785219998a60e44e18
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-04 00:16:50 +0900

    faker実装設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
     nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    email                 {Faker::Internet.free_email} # Fakerを使うのは一意性であるここのみで問題がないと考えられる
+    password              {'a00000'} # Faker::Alphanumeric.alphanumeric(number: 6, min_alpha: 1, min_numeric: 1) 6文字で最低1文字アルファベット最低1文字数値のFaker,コンソールで動作確認済み
     password_confirmation {password}
     family_name           {'田中'}
     first_name            {'太郎'}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
-      # nicknameが空では登録できないテストコードを記述します
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.nickname = ''  # nicknameの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include "Nickname can't be blank"
     end
     it 'emailが空では登録できない' do
       # emailが空では登録できないテストコードを記述します
