5c9d274252282e4d914288caa01b41a17bb9bb67
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-05 13:44:22 +0900

    itemモデルへのアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  belong_to :user
+
+  # 選択肢用のアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_state
+  belongs_to :delivery_charge
+  belongs_to :delivery_area
+  belongs_to :delivery_day_id
+
 end
