9c44fec0533b7397b4e74ddc139f95d4e00a58d5
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-05 19:52:19 +0900

    factoryでのテストデータ生成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image              {Faker::Lorem.sentence}
+    item_name          {'item'}
+    item_descripution  {'item_descripution'}
+    item_category_id   {1}
+    item_state_id      {1}
+    delivery_charge_id {1}
+    delivery_area_id   {2}
+    delivery_day_id    {1}
+    price              {500}
+    association :user
   end
 end
