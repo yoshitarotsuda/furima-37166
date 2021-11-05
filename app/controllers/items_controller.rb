63ed4d35d2a5f2684ed72be429f5ac413c30ab05
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-05 15:51:48 +0900

    createアクション実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,18 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item)
+  end
 end
