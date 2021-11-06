69538a31b3ca0b4b22acb4d2cbeaa0ccedda3bfa
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-06 14:55:01 +0900

    コントローラーのテストコードは不要
    原因にBASIC認証が考えられる

---
+++ spec/requests/items_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+# 課題提出では不要
+
+# describe ItemsController, type: :request do
+  
+#   before do
+#     @user = FactoryBot.create(:user)
+#   end
+  
+#   describe "GET #new" do
+#     it "newアクションにリクエストすると正常にレスポンスが返ってくる" do
+#       binding.pry
+#       sign_in @user
+      
+#       get new_item_path
+#       expect(response.status).to eq 200
+#     end
+#   end
+# end
