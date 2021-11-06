c69dc67ef880df1dff7fb7cd714ca36997502c85
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-06 11:39:31 +0900

    ログアウト状態の遷移設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,11 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new
+    if user_sign_in?
+      @item = Item.new
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def create
