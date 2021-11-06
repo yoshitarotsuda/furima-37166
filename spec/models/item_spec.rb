3c76cb67df42f9a4e8c42c23ec7efddfc3ed3448
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-06 11:31:08 +0900

    商品出品機能　itemモデルの単体テスト条件書き出し

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,20 +3,52 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
-    @item.image = fixture_file_upload('public/test.webp')
+    @item.image = fixture_file_upload('public/test.webp') #public1ディレクトリ の画像１ファイル参照
   end
-
   describe '商品出品' do
-    it '商品画像が空では登録できない' do # データがActiveStrageに格納できるか、サイズはどうかなどかは一時保留
-      # @item.image = ''
-      # @item.valid?
-      
-      # expect(@item.errors.full_messages).to include("Image can't be blank")
+    context '商品出品できるとき' do
+      it '以下の出品できない場合を満たさない場合登録できる' do
+        expect(@item).to be_valid
+      end
     end
-    it '商品名が空では登録できない' do
-      @item.item_name = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Item name can't be blank")
+    context '商品の出品ができる時'
+      it '商品画像が空では登録できない' do # データがActiveStrageに格納できるか、サイズはどうかなどかは一時保留
+        @item.image = nil # ''ではエラーが出る
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空では登録できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it '商品の説明が空では登録できない' do
+        
+      end
+      it 'カテゴリーが空または「---」では登録できない' do
+
+      end
+      it '商品の状態が空または「---」では登録できない' do
+        
+      end
+      it '配送料の負担が空または「---」では登録できない' do
+        
+      end 
+      it '配送元の地域が空または「---」では登録できない' do
+        
+      end
+      it '発送までの日数が空または「---」では登録できない' do
+        
+      end
+      it '価格の情報が空では登録できない' do
+        
+      end
+      it '価格の情報は半角数値でなければ登録できない' do
+        
+      end
+      it '価格の情報は300以上9999999以下でなければ登録できない' do
+        
+      end
     end
   end
 end
