f28efdd050b5d0cb85c57b440d28d71bf9b78f12
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-06 15:40:38 +0900

    デバッグコードの削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,9 @@
 //  一旦全ページで動いてしまうことは保留します
 
 window.addEventListener('load',() => {
-  // console.log("OK");
   const priceInput = document.getElementById("item-price");
-  // console.log(priceInput);
   priceInput.addEventListener("input",() => {
-    // console.log("イベント発火");
     const inputValue = priceInput.value;
-    // console.log(inputValue)
     const addTaxDom = document.getElementById("add-tax-price")
     addTaxDom.innerHTML = inputValue * 0.1
     const addProfit = document.getElementById("profit")
