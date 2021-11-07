0be441de94cd94b58f755458565a61e81df7a542
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-07 13:07:31 +0900

    updateアクション追加、リンク追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,9 @@ class ItemsController < ApplicationController
     end
   end
 
+  def update
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :item_name, :item_description, :item_category_id, :item_state_id, :delivery_charge_id, :delivery_area_id, :delivery_day_id, :price).merge(user_id: current_user.id)
