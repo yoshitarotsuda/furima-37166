43c57a0e6e8bc35cfecbb5166587d2fd654e8045
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-07 13:50:38 +0900

    ログアウトユーザーの遷移先指定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_user!, except: [:index,:show] # 現場リダイレクト先が確定してないので封鎖
+  before_action :authenticate_user!, except: [:index,:show] # ログアウトユーザーの移動先限定、指定対象外はroot_pathへ移動
   
   def index
     @items = Item.all.order("created_at DESC")
