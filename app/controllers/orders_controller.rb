c1796a358010f89173ea4d7eb5c0c015a8575c5d
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-08 18:16:35 +0900

    ログアウト状態の遷移設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,11 @@
 class OrdersController < ApplicationController
-
+  before_action :authenticate_user! # 注文関連の繊維は全て認めないのでこれで設定
+  before_action :set_item, only: [:index, :create]
   def index
-    @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new
   end
 
-  def create
+  def create 
     @order_delivery = OrderDelivery.new(order_params)
     if @order_delivery.valid?
       @order_delivery.save
@@ -16,9 +16,11 @@ class OrdersController < ApplicationController
   end
 
   private
-  
   def order_params
     params.require(:order_delivery).permit(:postal_code, :delivery_area_id, :city, :block, :building_name, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
   end
 
+  def set_item # privateなのでitems_controllerと同名でも問題なし
+    @item = Item.find(params[:item_id])
+  end
 end
