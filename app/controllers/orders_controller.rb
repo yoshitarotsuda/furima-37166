d128c561fd167b8c689a4537215eff400947b1e2
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-08 13:08:47 +0900

    注文情報の保存確認

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,8 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new
+    @order = Order.new(params.permit(:item_id).merge(user_id: current_user.id))
     @order.save
   end
+
 end
