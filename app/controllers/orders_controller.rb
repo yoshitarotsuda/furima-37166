f9a148b42bc6497357092ea71f59ab3d52e50418
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-08 15:58:26 +0900

    OrdersコントローラーをFormオブジェクト対応に変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,23 @@
 class OrdersController < ApplicationController
 
   def index
-    
+    @order_delivery = OrderDelivery.new
   end
 
   def create
-    order = Order.new(params.permit(:item_id).merge(user_id: current_user.id))
-    order.save
-    delivery_destination = DeliveryDestination.new(params.permit(:postal_code, :delivery_area_id, :city, :block, :building_name, :phone_number).merge(order_id: order.id))
-    delivery_destination.save
+    @order_delivery = OrderDelivery.new
+    if @order_delivery.valid?
+      @order_delivery.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+  
+  def order_params
+    params.require(:order_delivery).permit(:item_id, :postal_code, :delivery_area_id, :city, :block, :building_name, :phone_number)merge(user_id: current_user.id)
   end
 
 end
