93553389b1ae7aa31b2906888b82c8deee551f2a
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-09 17:51:16 +0900

    商品購入に必要な情報のFactoryBot記述

--- spec/factories/delivery_destinations.rb
+++ spec/factories/delivery_destinations.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :delivery_destination do
-    
+    postal_code {'000-0000'}
+    delivery_area_id {2}
+    city {'東京都'}
+    block {'0-0-0'}
+    building_name {'テストビル'}
+    token {"tok_e59d9bfaf22568bf466e2affd441"}
+    association :user
+    association :item
   end
 end
