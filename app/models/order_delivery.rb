c41305c62946cdf953b311a5ae1e94925f8a12bc
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-09 20:58:03 +0900

    テストコードの記述、バリデーションの記述変更

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -6,12 +6,13 @@ class OrderDelivery
     validates :user_id
     validates :item_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message:"○○○-○○○○の形式で入力してください"}
-    validates :delivery_area_id, numericality: { other_than: 0, message: "can't be blank"}
     validates :city
     validates :block
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message:"ハイフン無しの10文字から11文字の半角数値で入力してください"}
     validates :token
   end
+
+  validates :delivery_area_id, presence: true, numericality: { other_than: 0, message:"can't be blank" }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
---
+++ spec/factories/order_delivery.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :order_delivery do
+    postal_code {'000-0000'}
+    delivery_area_id {2}
+    city {'テスト区'}
+    block {'0-0-0'}
+    building_name {'テストビル'}
+    phone_number {'00000000000'}
+    token {"tok_e59d9bfaf22568bf466e2affd441"}
+    association :user
+    association :item
+  end
+end
