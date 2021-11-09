bf672168f69630c5f8c71e9c7e41b24fe121b3d1
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-09 15:32:08 +0900

    カード情報の削除確認

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -15,19 +15,17 @@ const pay = () => {
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token)
-        
-        // const renderDom = document.getElementById("charge-form");
-        // const tokenObj = `<input value=${token} name='token' type="hidden"> `;
-        // renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        debugger;
       }
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
 
-      // document.getElementById("order_number").removeAttribute("name");
-      // document.getElementById("order_cvc").removeAttribute("name");
-      // document.getElementById("order_exp_month").removeAttribute("name");
-      // document.getElementById("order_exp_year").removeAttribute("name");
-
-      // document.getElementById("charge-form").submit();
+      document.getElementById("charge-form").submit();
     });
 
   })
