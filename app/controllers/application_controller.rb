93cbc423b73d6a5f30ca4ec1641e99149e2b18ea
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-10 11:58:49 +0900

    何かのテストで削除していたBASIC認証再設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
-  # before_action :basic_auth
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
-  # def basic_auth # BASIC認証設定
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-  #   end
-  # end
+  def basic_auth # BASIC認証設定
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 
   def configure_permitted_parameters # メソッド名は慣例 sign_up 新規登録のストロングパラメーター
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
