e9761c40e4ea9658fcd3394f175ff0206ed9c548
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-11 11:22:51 +0900

    記述確認修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock "~> 3.16.0"
+lock '~> 3.16.0'
 
 # Capistranoのログの表示に利用する
-set :application, "furima-37166"
+set :application, 'furima-37166'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, "git@example.com:yoshitarotsuda/furima-37166.git"
+set :repo_url, 'git@example.com:yoshitarotsuda/furima-37166.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
