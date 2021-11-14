df72182f30f991e6679402e4e7d6832162576339
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-14 13:29:49 +0900

    データの保存確認、ストロングパラメーター、バリデーション設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+  end
+
+  private
+  def comment_params
+    params.permit(:comment).merge(user_id: current_user.id,item_id: params[:item_id])
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :comment
+  end
 end
